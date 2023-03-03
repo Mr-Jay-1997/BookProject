397c4dea64aae7f2ce5646b74c39b15820c56105
Jayadurga Prasad B <jayadurgaprasad226@neel-tech.international>
2023-03-03 11:50:56 +0530

    Add project files.

---
+++ BookProject/Controllers/CategoryController.cs
@@ -0,0 +1,90 @@
+﻿using BLLBookProject;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Models;
+
+namespace BookProject.Controllers
+{
+    [Authorize]
+    public class CategoryController : Controller
+    {
+
+        BLLCategory bllcategory = new BLLCategory();
+        public IActionResult Index()
+        {
+            var categoryList = bllcategory.GetCategoryList();
+            return View(categoryList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var category = bllcategory.GetCategory(id);
+            return View(category);
+        }
+
+        public IActionResult Create()
+        {
+            var categoryModel = new CategoryModel();
+            return PartialView("_AddPartialView",categoryModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryModel categoryModel)
+        {
+            try
+            {
+                categoryModel = bllcategory.CreateCategory(categoryModel);
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch
+            {
+
+            }
+            return View(categoryModel);
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var updatedCategory = new CategoryModel();
+            updatedCategory = bllcategory.GetCategory(id);
+            return PartialView("_EditPartialView", updatedCategory);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int categoryId, CategoryModel updatedCategory)
+        {
+            try
+            {
+                updatedCategory = bllcategory.UpdateCategory(categoryId, updatedCategory);
+
+            }
+            catch
+            {
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var categoryModel = new CategoryModel();
+            categoryModel = bllcategory.GetCategory(id);
+            return View(categoryModel);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id, CategoryModel categoryModel)
+        {
+            try
+            {
+                categoryModel = bllcategory.DeleteCategory(id, categoryModel);
+            }
+            catch
+            {
+                // Handle any exceptions
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ BookProject/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    $('#myTable').DataTable({
+        "scrollX": false,
+        "scrollY": false,
+        "scrollCollapse": true,
+        "paging": true,
+        "sWidth" : '10%'
+
+    });
+});
+
+$(document).ready(function () {
+    $('#myTable2').DataTable({
+        "scrollX": false,
+        "scrollY": false,
+        "scrollCollapse": true,
+        "paging": true,
+        "sWidth": '10%',
+        "columnDefs": [
+            {
+                "targets": [0],
+                "visible": false,
+                "searchable": false
+            },
+        ]
+    });
+});
